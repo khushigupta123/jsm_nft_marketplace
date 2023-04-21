44f1401123630988e021923540935a1732bee940
Gola-k <kunalgola426@gmail.com>
2023-04-21 17:13:52 +0530

    Complted Project before deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
+const dedicatedEndPoint = 'https://gola-nft-marketplace.infura-ipfs.io';
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: [dedicatedEndPoint, 'gola-nft-marketplace.infura-ipfs.io'],
+  },
+  env: {
+    BASE_URL: process.env.BASE_URL,
+  },
 }
 
 module.exports = nextConfig
