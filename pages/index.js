5f8659cb1f4674f1ec2c84187d57390a07bfe7d8
Gola-k <kunalgola426@gmail.com>
2023-05-12 10:57:52 +0530

    New NFT Added and Name changed to MediBlock

--- pages/index.js
+++ pages/index.js
@@ -105,7 +105,7 @@ const Home = () => {
     <div className="flex justify-center sm:px-4 p-12">
       <div className="w-full minmd:w-4/5">
         <Banner
-          name={(<>Discover, collect, and sell <br />extraordinary NFTs</>)}
+          name={(<>Empowering Healthcare with <br />NFT Tokens</>)}
           childStyles="md:text-4xl sm:text-2xl xs:text-xl text-left"
           parentStyles="justify-start mb-7 h-72 sm:h-60 p-12 xs:p-4 xs:h-44 rounded-3xl"
         />
@@ -152,7 +152,7 @@ const Home = () => {
 
             <div className="mt-10">
               <div className="flexBetween mx-4 xs:mx-0 minlg:mx-8 sm:flex-col sm:items-start">
-                <h1 className="flex-1 font-poppins dark:text-white text-nft-black-1 text-2xl minlg:text-4xl font-semibold sm:mb-4">Hot NFTs</h1>
+                <h1 className="flex-1 font-poppins dark:text-white text-nft-black-1 text-2xl minlg:text-4xl font-semibold sm:mb-4">Hot MediBlocks</h1>
                 <div className="flex-2 sm:w-full flex flex-row sm:flex-col">
                   <SearchBar
                     activeSelect={activeSelect}
